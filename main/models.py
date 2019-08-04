9a214bcc3d24414ceea441a0c9d1ca69540e0a36
Kaustubh Dixit <kdkishu335@gmail.com>
2019-08-04 11:25:12 +0530

    Improved functionality

--- main/models.py
+++ main/models.py
@@ -16,6 +16,9 @@ class BookOfTheMonth(models.Model):
     review = models.TextField(max_length=100, blank=False)
     active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.book.name
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=32, blank=False, default='name')
